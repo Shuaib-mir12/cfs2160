b948d40a043fb778dc478850a128d84400e21a1c
Shuaib-mir12 <king_shuaib@live.com>
2019-12-19 14:36:33 +0000

    Add files via upload

--- lab divide.py
+++ lab divide.py
@@ -1,20 +1,20 @@
-number_of_pcs_in_lab = 24
+pcs_in_lab = 24
 
 try:
-    number_of_students = int (input ('Enter the number of students: '))
+    number_of_students = int (input ('Enter the amount of students: '))
 
     if number_of_students < 0:
-        print ('Error: A positive number of students is required')
+        print ('Error: please input a positive number of students')
     else:
-        number_of_labs = number_of_students // number_of_pcs_in_lab
+        number_of_labs = number_of_students // pcs_in_lab
 
-        if number_of_students % number_of_pcs_in_lab != 0:
+        if number_of_students % pcs_in_lab != 0:
             number_of_labs += 1
 
         if number_of_labs == 1:
-            print (number_of_labs, 'is required to hold this many students')
+            print ('you need ' + number_of_labs.__str__() + ' lab to hold this many students')
         else:
-            print (number_of_labs, 'is required to hold this many students')
+            print ('you need ' + number_of_labs.__str__() +  ' labs to hold this many students')
 
 except ValueError:
     print ('Please enter a valid number.')
